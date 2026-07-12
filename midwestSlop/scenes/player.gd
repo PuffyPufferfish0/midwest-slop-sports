e3fded29ab955da0c389485463737097bd909c53
Jack_Anderson <Anderson.692@wright.edu>
2026-07-12 00:58:00 -0400

    simulated multiplayer working. tomorrow work on camera

--- midwestSlop/scenes/player.gd
+++ midwestSlop/scenes/player.gd
@@ -1,13 +1,39 @@
 extends CharacterBody3D
 
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+# This grabs the camera from your node tree
+@onready var camera = $SpringArm3D/Camera3D
+
 func _enter_tree():
-	#player network ID
 	set_multiplayer_authority(name.to_int())
 
+func _ready():
+	if is_multiplayer_authority():
+		camera.current = true
+	else:
+		camera.current = false
+
 func _physics_process(delta):
-	# If this isn't our character, don't let us move it.
 	if not is_multiplayer_authority():
 		return
-		
-	# movement
+
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
