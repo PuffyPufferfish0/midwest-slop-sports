61a2b56a2f5214dc7ee26e4593b7049a870dc775
Jack_Anderson <Anderson.692@wright.edu>
2026-07-11 23:47:13 -0400

    made project in Godot

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixgl.url = "github:nix-community/nixGL";
   };
 
   outputs =
@@ -11,19 +12,30 @@
       let
         pkgs = (
           import (inputs.nixpkgs) {
-
             inherit system;
             config = {
               allowUnfree = true;
             };
           }
         );
+		
+		#this is AI, just for me to run with nixGL
+        nixglPkgs = inputs.nixgl.packages.${system} or {};
       in
       {
         devShell = pkgs.mkShell {
           buildInputs = with pkgs; [
             godot
+          ] ++ lib.optionals stdenv.isLinux [
+		  
+            nixglPkgs.nixGLDefault
           ];
+
+          shellHook = ''
+            ${pkgs.lib.optionalString pkgs.stdenv.isLinux ''
+              alias godot="nixGL godot"
+            ''}
+          '';
         };
       }
     );
